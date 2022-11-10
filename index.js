1271b8b0e63977078e233f6e37dfeea0eddfddc4
GFioriUNNOBA <fiorigabriel7@gmail.com>
2022-11-10 11:50:26 -0300

    intro-uno-js : este es mi primer ejercicio en JS

---
+++ index.js
@@ -0,0 +1,11 @@
+let ciudad= "chacabuco";
+let numeroRandom= 5
+numeroRandom=ciudad
+console.log(numeroRandom);
+console.log(ciudad);
+
+const nombre = "Gabriel"
+const apellido ="Fiori"
+
+let text = `OMG ${nombre} ${apellido},Hi`;
+console.log(text);
